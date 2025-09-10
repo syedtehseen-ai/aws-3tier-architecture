4a834c568753c34aba0c73623004ef55a3e321f3
syedtehseen-ai <syed@syed.com>
2025-09-10 17:41:55 +0000

    initial draft by excluuding cdn

--- modules/presentation/s3.tf
+++ modules/presentation/s3.tf
@@ -32,16 +32,24 @@ resource "aws_s3_bucket_ownership_controls" "site" {
 resource "aws_s3_bucket_public_access_block" "site" {
   bucket = aws_s3_bucket.site.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true # Block ACLs
+  block_public_policy     = false # Allow public bucket policy
+  ignore_public_acls      = true
+  restrict_public_buckets = false # must be false, otherwise policy won’t work
 }
 
-resource "aws_s3_bucket_acl" "site" {
-  depends_on = [aws_s3_bucket_ownership_controls.site, aws_s3_bucket_public_access_block.site]
-  bucket     = aws_s3_bucket.site.id
-  acl        = "public-read"
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.site.arn}/*"
+    }]
+  })
 }
 
 # static website configuration
